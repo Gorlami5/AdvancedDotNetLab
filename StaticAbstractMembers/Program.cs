d3c2785ee412d4c3d7b614bf4c505d98deb3d117
Muhammet Yonkuc <muhammetyonkuc99@gmail.com>
2026-01-11 22:24:22 +0300

    Static abstract members

---
+++ StaticAbstractMembers/Program.cs
@@ -0,0 +1,15 @@
+﻿IInterface.X();
+//IInterface.Y(); eğer abstract keyword'ünü kullanırsak direkt olarka tip üzeridnen erişimimiz olmayacaktır.
+class MyClass : IInterface
+{
+    public static void Y()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+interface IInterface
+{
+    public static void X() { } // burada görüldüğü üzere static member'lar interface içerisinde scope ihtiyacı vardır.
+    public abstract static void Y(); // eğer static bir class için implementasyonu zorunlu tutmak istiyorsak abstract keywordü kullanmamız yeterli olacaktır.
+}
